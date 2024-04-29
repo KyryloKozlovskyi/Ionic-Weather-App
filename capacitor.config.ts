06b30500c441d0d3c5c517875e6f4af14a51b167
Kyrylo Kozlovskyi <G00425385@atu.ie>
2024-04-29 22:30:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-Weather-App',
+  webDir: 'www'
+};
+
+export default config;
