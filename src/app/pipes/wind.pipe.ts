95fff5f9ec8230427b2f0f8850fe6ef7a4573a7a
KYRYLO KOZLOVSKYI <G00425385@atu.ie>
2024-05-04 21:19:00 +0100

    Wind pipe updated

--- src/app/pipes/wind.pipe.ts
+++ src/app/pipes/wind.pipe.ts
@@ -7,14 +7,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class WindPipe implements PipeTransform {
 
   transform(degrees: number): string {
-    if (degrees >= 0 && degrees < 90) {
-      return 'Northeast';
-    } else if (degrees >= 90 && degrees < 180) {
-      return 'Southeast';
-    } else if (degrees >= 180 && degrees < 270) {
-      return 'Southwest';
-    } else if (degrees >= 270 && degrees < 360) {
-      return 'Northwest';
+    if (degrees >= 0 && degrees < 45) {
+      return 'N';
+    } else if (degrees >= 45 && degrees < 90) {
+      return 'NE';
+    } else if (degrees >= 90 && degrees < 135) {
+      return 'E';
+    } else if (degrees >= 135 && degrees < 180) {
+      return 'SE';
+    } else if (degrees >= 180 && degrees < 225) {
+      return 'S';
+    } else if (degrees >= 225 && degrees < 270) {
+      return 'SW';
+    } else if (degrees >= 270 && degrees < 315) {
+      return 'W';
+    } else if (degrees >= 315 && degrees < 360) {
+      return 'NW';
     } else {
       return 'Invalid degrees';
     }
