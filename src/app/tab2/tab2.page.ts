a3a7601293a62bd58f0dabb1d626bd3defdd5cce
KYRYLO KOZLOVSKYI <G00425385@atu.ie>
2024-05-06 19:41:31 +0100

    Geocoding service

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { IonSearchbar } from '@ionic/angular/standalone';
+import { GeocodingService } from '../services/geocoding.service';
 
 @Component({
   selector: 'app-tab2',
@@ -11,7 +12,18 @@ import { IonSearchbar } from '@ionic/angular/standalone';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar]
 })
 export class Tab2Page {
+  public resp: any = []; // Stores jsonpublic resp: any = []; // Stores json
+  constructor(private geocodingService: GeocodingService) {}
 
-  constructor() {}
+  // Calls an api on page initialization
+  async ngOnInit(){
+    await this.getGeocoding();
+  }
 
+  async getGeocoding(){
+    this.geocodingService.getGeocoding("Galway").subscribe((response) => {
+    this.resp = response;
+    console.log(this.resp); // Logs json to the console
+  });
+}
 }
