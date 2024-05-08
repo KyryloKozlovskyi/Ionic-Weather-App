63dcef530a8a32f2ff9722bcea28609b708be74e
KYRYLO KOZLOVSKYI <G00425385@atu.ie>
2024-05-08 01:12:58 +0100

    Input error handling settings page

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -15,6 +15,7 @@ import { StorageService } from '../services/storage.service';
 import { AsyncPipe } from '@angular/common';
 import { Geolocation } from '@capacitor/geolocation';
 import { ReverseService } from '../services/reverse.service';
+import { AlertController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab4',
@@ -47,7 +48,8 @@ export class Tab4Page implements OnInit {
   constructor(
     private geocodingService: GeocodingService,
     private storageService: StorageService,
-    private reverseWeatherService: ReverseService
+    private reverseWeatherService: ReverseService,
+    private alertController: AlertController
   ) {}
 
   async ngOnInit() {
@@ -63,7 +65,7 @@ export class Tab4Page implements OnInit {
       console.log('Input changed (settings):', value);
       this.userInput = value;
       this.storageService.set('defaultSetting', this.userInput);
-      if(this.userInput != "Default"){
+      if (this.userInput != 'Default') {
         await this.getGeocoding(this.userInput);
       }
       await this.getGPS();
@@ -76,7 +78,7 @@ export class Tab4Page implements OnInit {
       this.coordinates = await Geolocation.getCurrentPosition();
       this.lon = this.coordinates.coords.longitude;
       this.lat = this.coordinates.coords.latitude;
-  
+
       this.reverseWeatherService
         .getReverseGeocoding(this.lat, this.lon)
         .subscribe(async (response) => {
@@ -85,7 +87,7 @@ export class Tab4Page implements OnInit {
             const defaultSetting = await this.storageService.get(
               'defaultSetting'
             );
-  
+
             if (!defaultSetting) {
               await this.storageService.set('defaultSetting', 'Default');
               const cityName = this.geoRevResp[0]?.name;
@@ -105,16 +107,46 @@ export class Tab4Page implements OnInit {
       console.error('Error getting GPS coordinates:', error);
     }
   }
-  
-  // API Call. Translates city name into coordinates. Calls the api to get weather data for the specified city
+
   async getGeocoding(input: any) {
-      this.geocodingService.getGeocoding(input).subscribe(async (response) => {
-      this.geoResp = response;
-      this.lat = this.geoResp[0].lat;
-      this.lon = this.geoResp[0].lon;
-      console.log(this.geoResp); // Logs json to the console
-      console.log(this.lat); // Logs lat
-      console.log(this.lon); // Logs lon
-    });
+    this.geocodingService.getGeocoding(input).subscribe(
+      async (response) => {
+        if (response && response.length > 0) {
+          this.geoResp = response;
+          this.lat = this.geoResp[0].lat;
+          this.lon = this.geoResp[0].lon;
+          console.log(this.geoResp); // Logs json to the console
+          console.log(this.lat); // Logs lat
+          console.log(this.lon); // Logs lon
+        } else {
+          // Handle the case when response is undefined or empty
+          console.error('Error in geocoding service: Check your input.');
+          const alert = await this.alertController.create({
+            header: 'Error',
+            message: 'Check your input.',
+            buttons: ['OK'],
+          });
+          await alert.present();
+          await this.storageService.set('defaultSetting', 'Default');
+          await this.storageService.set('defaultLocation', 'Default');
+          this.defaultSetting = await this.storageService.get('defaultSetting');
+          await this.getGPS();
+        }
+      },
+      async (error) => {
+        // Handle the error here
+        console.error('Error in geocoding service:', error);
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: 'An error occurred..',
+          buttons: ['OK'],
+        });
+        await alert.present();
+        await this.storageService.set('defaultSetting', 'Default');
+        await this.storageService.set('defaultLocation', 'Default');
+        this.defaultSetting = await this.storageService.get('defaultSetting');
+        await this.getGPS();
+      }
+    );
   }
 }
